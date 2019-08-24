b7d3fb2d3158dfbe9bff027bf4186d024ade14d7
Simon <chechin.simon@gmail.com>
2019-08-24 17:26:22 +0300

    .env

--- zmanim_bot/bot.py
+++ zmanim_bot/bot.py
@@ -4,21 +4,28 @@ import logging
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_polling, set_webhook
+from aiogram.contrib.fsm_storage.redis import RedisStorage2
+from aiogram.dispatcher.storage import FSMContext
 
 from settings import BOT_TOKEN
 from zmanim_bot.text_handler import TextHandler
+from os import environ as env
 
+from zmanim_bot.states import UserStates as User
 
 logging.basicConfig(level=logging.INFO)
 
 loop = asyncio.get_event_loop()
-bot = Bot(BOT_TOKEN, loop, parse_mode=types.ParseMode.MARKDOWN)
-dp = Dispatcher(bot)
+bot = Bot(env['BOT_TOKEN'], loop, parse_mode=types.ParseMode.MARKDOWN)
+
+storage = RedisStorage2(host=env['redis_host'], port=env['redis_port'])
+dp = Dispatcher(bot, storage=storage, loop=loop)
 
 
 @dp.message_handler(commands=['start'])
 async def handle_start(msg: types.Message):
     response = 'start'
+    User.MainMenu.set()
     await bot.send_message(msg.chat.id, response)
 
 
@@ -28,8 +35,8 @@ async def handle_start(msg: types.Message):
     await bot.send_message(msg.chat.id, response)
 
 
-@dp.message_handler(content_types=types.ContentTypes.TEXT)
-async def handle_start(msg: types.Message):
+@dp.message_handler(content_types=types.ContentTypes.TEXT, state=User.MainMenu)
+async def handle_start(msg: types.Message, state: FSMContext):
     th = await TextHandler.create(msg.chat.id, msg.text)
     #  TODO call __init__ synchronously
     await th.process_text()
@@ -37,5 +44,7 @@ async def handle_start(msg: types.Message):
 
 # todo handlers
 
+if __name__ == '__main__':
 
-start_polling(dp)
+    print (env['BOT_TOKEN'])
+    #start_polling(dp)
--- zmanim_bot/states.py
+++ zmanim_bot/states.py
@@ -1,67 +1,7 @@
-import redis
+from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
-redis_host = "localhost"
-redis_port = 6379
-redis_password = ""
-
-
-class States:
-    def __init__(self, user_id: int, state: str):
-        self.user_id = user_id
-        self.state = state
-
-    @classmethod
-    async def create(cls, user_id: int, state: str):
-        """
-        Creates TextHandler instance asynchronously for text handling
-        :param user_id: telegram id
-        :param text: user's message
-        :return: TextHandler instance
-        """
-        self = States(user_id, state)
-        return self
-
-
-    @staticmethod
-    async def new_user(user_id: int):
-        con = redis.StrictRedis(host=redis_host, port=redis_port,
-                                  password=redis_password, decode_responses=True)
-
-        con.set(f'{user_id}', "start")
-
-
-    @staticmethod
-    async def set_state(user_id: int, state: str):
-        con = redis.StrictRedis(host=redis_host, port=redis_port,
-                                password=redis_password, decode_responses=True)
-
-        con.set(f'{user_id}', state)
-
-
-
-
-def hello_redis():
-    """Example Hello Redis Program"""
-
-    # step 3: create the Redis Connection object
-    try:
-
-        # The decode_repsonses flag here directs the client to convert the responses from Redis into Python strings
-        # using the default encoding utf-8.  This is client specific.
-        r = redis.StrictRedis(host=redis_host, port=redis_port,
-                              password=redis_password, decode_responses=True)
-
-        # step 4: Set the hello message in Redis
-        r.set("msg:hello", "Hello Redis!!!")
-
-        # step 5: Retrieve the hello message from Redis
-        msg = r.get("msg:hello")
-        print(msg)
-
-    except Exception as e:
-        print(e)
-
-
-if __name__ == '__main__':
-    hello_redis()
+class UserStates:
+    MainMenu = State()
+    Holidays = State()
+    Location = State()
