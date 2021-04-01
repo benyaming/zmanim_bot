d7b8803dcc4e79d247a638ec1b3f0f1e7f74ad86
Benyamin Ginzburg <benyomin.94@gmail.com>
2021-04-01 10:52:24 +0300

    Add broadcaster

---
+++ broadcast/data.py
@@ -0,0 +1,8 @@
+ru_message = '''тестовая рассылка'''
+
+en_message = '''broadcast test'''
+
+ru_button_text = 'Поддержка zmanim_bot'
+en_button_text = 'Donate to zmanim_bot'
+
+link = 'https://sobe.ru/na/zmanim_bot'
