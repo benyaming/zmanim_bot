102a8a71c670f3fa3bb81ad7a410e499e11abcf8
Benyamin Ginzburg <benyomin.94@gmail.com>
2021-04-01 11:06:15 +0300

    Update data.py

--- broadcast/data.py
+++ broadcast/data.py
@@ -5,4 +5,4 @@ en_message = '''broadcast test'''
 ru_button_text = 'Поддержка zmanim_bot'
 en_button_text = 'Donate to zmanim_bot'
 
-link = 'https://sobe.ru/na/zmanim_bot'
+link = 'https://telegra.ph/Donate-to-Zmanim-bot-03-31'
