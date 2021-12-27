724dd0e63dd940eaea63a6c09bada493996907a7
Benyamin Ginzburg <benyomin.94@gmail.com>
2021-12-27 10:44:50 +0200

    Fix bug with Done button

--- zmanim_bot/handlers/forms.py
+++ zmanim_bot/handlers/forms.py
@@ -10,7 +10,7 @@ from zmanim_bot.keyboards.menus import get_report_keyboard
 from zmanim_bot.misc import bot
 from zmanim_bot.service import converter_service, settings_service, zmanim_service
 from zmanim_bot.states import FeedbackState
-from zmanim_bot.texts.single import messages
+from zmanim_bot.texts.single import messages, buttons
 from zmanim_bot.utils import chat_action
 
 
@@ -89,17 +89,20 @@ async def handle_location_name(msg: Message, state: FSMContext):
     redirect_target = state_data.get('redirect_target', 'main')
     redirect_message = state_data.get('redirect_message')
     origin_message_id = state_data.get('origin_message_id')
-
-    location_kb = await settings_service.update_location_name(new_name=msg.text, old_name=old_name)
-
-    if origin_message_id:
-        await bot.edit_message_reply_markup(msg.from_user.id, origin_message_id, reply_markup=location_kb)
-
     targets = {
         'main': redirect_to_main_menu,
         'settings': redirect_to_settings_menu
     }
     redirect = targets[redirect_target]
+
+    if msg.text == buttons.done.value:
+        await state.finish()
+        return await redirect(redirect_message)
+
+    location_kb = await settings_service.update_location_name(new_name=msg.text, old_name=old_name)
+
+    if origin_message_id:
+        await bot.edit_message_reply_markup(msg.from_user.id, origin_message_id, reply_markup=location_kb)
 
     await state.finish()
     await redirect(redirect_message)
