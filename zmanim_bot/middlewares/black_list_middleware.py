d4c7f7986d1a2de7c27c01a2fe42b9059fde91af
Benyamin Ginzburg <benyomin.94@gmail.com>
2023-01-19 18:59:36 +0200

    Fix incorrect chat type call

--- zmanim_bot/middlewares/black_list_middleware.py
+++ zmanim_bot/middlewares/black_list_middleware.py
@@ -1,3 +1,4 @@
+from aiogram import types
 from aiogram.dispatcher.middlewares import BaseMiddleware
 
 from zmanim_bot.exceptions import AccessDeniedException
@@ -9,7 +10,15 @@ class BlackListMiddleware(BaseMiddleware):
         if action not in ('pre_process_message', 'pre_process_callback_query'):
             return
 
-        elif args[0].chat.type != 'private':
+        match args[0]:
+            case types.Message() as msg:
+                chat_type = msg.chat.type
+            case types.CallbackQuery() as call:
+                chat_type = call.message.chat
+            case _:
+                return
+
+        if chat_type != 'private':
             return
 
         from zmanim_bot.repository.bot_repository import get_or_create_user
